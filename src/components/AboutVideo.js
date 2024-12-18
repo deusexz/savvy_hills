b2e13f470c631a08a25df322df794e6c6db186cc
deusEcx <davidhayrapet13@gmail.com>
2024-12-18 12:55:35 +0400

    backup

--- src/components/AboutVideo.js
+++ src/components/AboutVideo.js
@@ -2,8 +2,21 @@ import React from 'react';
 import '../assets/css/about.css'
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faPlay} from "@fortawesome/free-solid-svg-icons/faPlay";
+import Slider from "react-slick";
+import DATA from "../data/aboutSlider";
 
 function AboutVideo(props) {
+
+    const settings = {
+        arrows:false,
+        dots:false,
+        infinite:true,
+        autoplay:true,
+        pauseOnHover: false,
+        slidesToShow:5,
+        slidesToScroll:1,
+    }
+
     return (
         <section className={'about'}>
             <div className={'aboutVideoWrapper'}>
@@ -16,6 +29,18 @@ function AboutVideo(props) {
                     <h1>Integer vel ultrices ipsum.</h1>
                     <p>Lorem ipsum dolor sit amet. Integer vel ultrices ipsum. Sed elit elit, fringilla a lobortis ut, faucibus nec ante. Nullam maximus tempus urna, eget luctus justo ultricies sit amet. Donec varius lacinia dictum. Proin rutrum dui gravida mauris pellentesque, quis viverra metus finibus. Sed elit elit, fringilla a lobortis ut.</p>
                 </div>
+            </div>
+            <h1 className={'aboutSliderHeader'}>Our Partners</h1>
+            <div className={'aboutSlider'}>
+                <Slider {...settings}>
+                    {
+                        DATA.map((item) => (
+                            <div className={'aboutSliderItem'} key={item.id}>
+                                <FontAwesomeIcon className={'sliderIcon'} icon={item.icon}/>
+                            </div>
+                        ))
+                    }
+                </Slider>
             </div>
         </section>
     );
---
+++ src/data/aboutSlider.js
@@ -0,0 +1,30 @@
+import {faGoogle, faAmazon, faGithub, faMicrosoft, faXbox, faApple, faFirefox, faTeamspeak} from "@fortawesome/free-brands-svg-icons";
+
+const DATA = [{
+    id: 1,
+    icon: faGoogle
+},{
+    id: 2,
+    icon: faAmazon
+},{
+    id: 3,
+    icon: faGithub
+},{
+    id: 4,
+    icon: faMicrosoft
+},{
+    id: 5,
+    icon: faApple
+},{
+    id: 6,
+    icon: faXbox
+},{
+    id: 7,
+    icon: faFirefox
+}, {
+    id: 8,
+    icon: faTeamspeak
+}
+]
+
+export default DATA;
