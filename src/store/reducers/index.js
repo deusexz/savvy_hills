562ad1b214d8872614504b56ff4126f147c93ec5
deusEcx <davidhayrapet13@gmail.com>
2025-03-01 11:37:20 +0400

    Updates

--- src/store/reducers/index.js
+++
@@ -1,9 +0,0 @@
-import {combineReducers} from "redux";
-import menuReducer from "./menuReducer";
-import contactBlockReducer from "./contactBlockReducer";
-
-const combine = combineReducers({
-    menuReducer, contactBlockReducer
-})
-
-export default combine;
